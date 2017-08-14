74ac9508ceab691db600def3d41dc4510b6cf074
JanVrba <jan.vrba@gmx.co.uk>
2017-08-14 16:50:05 +0200

    Update test.groovy

--- org/jvrba/test.groovy
+++ org/jvrba/test.groovy
@@ -1,1 +1,13 @@
-test
+package or.jvrba;
+  class acme implements Serializable {
+      private String name
+      def setName(value) {
+          name = value
+      }
+      def getName() {
+          name
+      }
+      def caution(message) {
+          echo "Hello, ${name}! CAUTION: ${message}"
+      }
+}
